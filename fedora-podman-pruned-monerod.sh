21ce85976d701929f17958b7d70c5855cd6a863c
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2023-08-18 10:33:23 -0300

    change podman container to user level, remove watch tower, add autoupdate flag

--- fedora-podman-pruned-monerod.sh
+++ fedora-podman-pruned-monerod.sh
@@ -2,9 +2,9 @@
 
 ## A script to run a pruned monero node with podman on Fedora, using sethforprivacy docker image https://hub.docker.com/r/sethsimmons/simple-monerod ##
 
-## Update your system and install curl ##
+## Update your system, install ufw and curl ##
 
-echo "Update your system and install curl"
+echo "Update your system, install ufw and curl"
 sudo dnf update -y
 sudo dnf install -y ufw curl
 
@@ -28,6 +28,7 @@ sudo ufw enable
 podman create --name=monerod --restart=always \
     -p 18080:18080 -p 18089:18089 \
     -v bitmonero:/home/monero \
+    -l "io.containers.autoupdate=registry" \
     docker.io/sethsimmons/simple-monerod:latest \
     --rpc-restricted-bind-ip=0.0.0.0 \
     --rpc-restricted-bind-port=18089 \
@@ -36,33 +37,9 @@ podman create --name=monerod --restart=always \
 ## Create systemd service unit file for monerod ##
 podman generate systemd --new --name monerod | sudo tee /etc/systemd/system/container-monerod.service
 
-## Enable and start the monerod systemd service ##
-sudo systemctl enable container-monerod.service
-sudo systemctl start container-monerod.service
-
-## ATÉ AQUI OK ##
-
-    
-## Start Podman Service: Enable and start the Podman service so that the Podman socket becomes available ##  
-sudo systemctl enable --now podman
-sudo systemctl start podman
-
-## Running Watchtower for Automatic Container Updates ##
-## Only works with sudo ##
-sudo podman create --name=watchtower --restart=always \
-    -v /var/run/podman/podman.sock:/var/run/podman/podman.sock \
-    docker.io/containrrr/watchtower --cleanup \
-    --monitor-only \
-    --interval 300 \
-    monerod tor
-    
-## Create systemd service unit file for Watchtower ##
-## Only works with sudo ##
-sudo podman generate systemd --new --name watchtower | sudo tee /etc/systemd/system/container-watchtower.service
-
-## Enable and start the watchtower systemd service ##
-sudo systemctl enable container-watchtower.service
-sudo systemctl start container-watchtower.service
+# Enable and start the monerod systemd service at user level
+systemctl --user enable /etc/systemd/system/container-monerod.service
+systemctl --user start container-monerod.service
 
 
 ## If you want to run a public pruned node comment the 2 commands above, and uncomment the commands below ##
@@ -73,16 +50,12 @@ sudo systemctl start container-watchtower.service
 #    --rpc-restricted-bind-ip=0.0.0.0 \
 #    --rpc-restricted-bind-port=18089 \
 #    --public-node --no-igd --no-zmq --enable-dns-blocklist --prune-blockchain
-#
-#sudo podman create --name=watchtower --restart=always \
-#    -v /var/run/podman/podman.sock:/var/run/podman/podman.sock \
-#    docker.io/containrrr/watchtower --cleanup \
-#    --monitor-only \
-#    --interval 300 \
-#    monerod tor
 
 ## To watch the logs for monerod, simply run ##
-journalctl -u container-monerod.service -f
+journalctl --user-unit=container-monerod.service -f
 
 ## To check the status of the monerod, simply run ##
-#sudo podman exec -it monerod monerod status
+#podman exec -it monerod monerod status
+
+## To check for updates, simply run ##
+#podman auto-update
